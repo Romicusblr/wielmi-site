41a154dc54b7242b698cdd014abd8f5ec6157413
Raman Mastyka <raman.mastyka@gmail.com>
2024-01-14 11:11:06 +0100

    refactor: refactor layout

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,30 +1,12 @@
 "use client";
-import classNames from "classnames";
-import React, { FC, useEffect, useState } from "react";
 import Hero from "../components/Hero/Hero";
 
-
-export default function Page(): JSX.Element {
-  const [currentSlide, setCurrentSlide] = useState(1);
-  const [nextSlide, setNextSlide] = useState<number | null>(null);
-
-  useEffect(() => {
-    if (nextSlide !== null) {
-      const timer = setTimeout(() => {
-        setCurrentSlide(nextSlide);
-        setNextSlide(null);
-      }, 250);
-      return () => clearTimeout(timer);
-    }
-  }, [nextSlide]);
-
-  const handleChangeSlide = (newSlide: number) => () => {
-    setNextSlide(newSlide);
-  };
-
+const Page = () => {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <Hero />
     </main>
   );
-}
+};
+
+export default Page;
