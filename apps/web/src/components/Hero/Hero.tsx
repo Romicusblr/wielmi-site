36c404eadcfe680c1b2431ccad63acd46947c8bb
Raman Mastyka <raman.mastyka@gmail.com>
2024-01-14 15:29:41 +0100

    fix: update styles

--- apps/web/src/components/Hero/Hero.tsx
+++ apps/web/src/components/Hero/Hero.tsx
@@ -28,46 +28,48 @@ const Hero: FC = () => {
           alt="cozy room"
         />
       ))}
-      <div className="absolute inset-0 flex flex-col pl-36">
+      <div className="absolute inset-0 flex flex-col">
         <div className="flex flex-col items-center mb-32 mt-8">
           <h1 className="text-4xl">WIELMI</h1>
           <h2>Inteligentny dom</h2>
         </div>
-        <ul className="text-4xl mb-8 w-1/2">
-          {slides.map(({ id, text }) => {
-            const isActive = currentSlide === id;
-            return (
-              <li
-                className={classNames({
-                  "text-3xl mb-4 flex justify-between": true,
-                  "text-gray-100/50": !isActive,
-                  "text-gray-100/100": isActive,
-                })}
-                key={id}
-              >
-                <Link onClick={() => setCurrentSlide(id)} href="#" scroll={false}>
-                  {text}
-                </Link>
-                <Link
+        <div className="pl-32">
+          <ul className="text-4xl mb-8 w-1/2">
+            {slides.map(({ id, text }) => {
+              const isActive = currentSlide === id;
+              return (
+                <li
                   className={classNames({
-                    "text-sm rounded-full border px-4 py-2": true,
-                    "hidden": !isActive,
+                    "text-3xl mb-4 flex justify-between": true,
+                    "text-gray-100/50": !isActive,
+                    "text-gray-100/100": isActive,
                   })}
-                  href="#"
+                  key={id}
                 >
-                  Więcej
-                </Link>
+                  <Link onClick={() => setCurrentSlide(id)} href="#" scroll={false}>
+                    {text}
+                  </Link>
+                  <Link
+                    className={classNames({
+                      "text-sm rounded-full border px-4 py-2": true,
+                      hidden: !isActive,
+                    })}
+                    href="#"
+                  >
+                    Więcej
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <ul className="flex items-center justify-between w-1/4">
+            {slides.map(({ id }) => (
+              <li key={id}>
+                <CircleButton onClick={() => setCurrentSlide(id)} isActive={currentSlide === id} text={id.toString()} />
               </li>
-            );
-          })}
-        </ul>
-        <ul className="flex items-center justify-between w-1/4">
-          {slides.map(({ id }) => (
-            <li key={id}>
-              <CircleButton onClick={() => setCurrentSlide(id)} isActive={currentSlide === id} text={id.toString()} />
-            </li>
-          ))}
-        </ul>
+            ))}
+          </ul>
+        </div>
         <div className="self-end w-1/2 px-16">
           <h3 className="mb-4">Dlaczego Wielmi?</h3>
           <p>
