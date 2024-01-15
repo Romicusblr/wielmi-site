d5178cb16da821aa0fbe4ae116e75342e08cebf1
Raman Mastyka <raman.mastyka@gmail.com>
2024-01-15 11:07:18 +0100

    build: update file names

---
+++ apps/web/src/components/hero/circle-button.tsx
@@ -0,0 +1,32 @@
+import classNames from "classnames";
+import type { FC } from "react";
+
+interface CircleButtonProps {
+  isActive: boolean;
+  text: string;
+  onClick: () => void;
+  className?: string;
+}
+
+const CircleButton: FC<CircleButtonProps> = ({
+  isActive,
+  text,
+  onClick,
+  className,
+}) => {
+  const buttonClass = classNames({
+    className,
+    "bg-white text-gray-800": isActive,
+    "bg-transparent text-white": !isActive,
+    "font-bold py-2 px-4 rounded-full transition ease-in-out duration-300 border border-white":
+      true,
+  });
+
+  return (
+    <button onClick={onClick} className={buttonClass}>
+      {text}
+    </button>
+  );
+};
+
+export default CircleButton;
---
+++ apps/web/src/components/hero/hero.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import classNames from "classnames";
+import type { FC } from "react";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import CircleButton from "./circle-button";
+
+const slides = [
+  { id: 1, text: "INTELIGENTNY DOM", src: "/wielmi-1.png" },
+  { id: 2, text: "INSTALACJE ELEKTRYCZNE", src: "/wielmi-2.png" },
+  { id: 3, text: "SIECI LOKALNE", src: "/wielmi-3.png" },
+];
+
+const Hero: FC = () => {
+  const [currentSlide, setCurrentSlide] = useState(slides[0].id);
+
+  return (
+    <div className="container relative w-full h-[720px]">
+      {slides.map(({ id, src }, i) => (
+        <Image
+          key={id}
+          className={classNames({
+            "absolute object-cover transition-opacity duration-500 brightness-[.5]": true,
+            "opacity-0": currentSlide !== id,
+          })}
+          fill
+          priority={i === 0}
+          src={src}
+          alt="cozy room"
+        />
+      ))}
+      <div className="absolute inset-0 flex flex-col">
+        <div className="flex flex-col items-center mb-32 mt-8">
+          <h1 className="text-4xl">WIELMI</h1>
+          <h2>Inteligentny dom</h2>
+        </div>
+        <div className="pl-32">
+          <ul className="text-4xl mb-8 w-1/2">
+            {slides.map(({ id, text }) => {
+              const isActive = currentSlide === id;
+              return (
+                <li
+                  className={classNames({
+                    "text-3xl mb-4 flex justify-between": true,
+                    "text-gray-100/50": !isActive,
+                    "text-gray-100/100": isActive,
+                  })}
+                  key={id}
+                >
+                  <Link
+                    onClick={() => {
+                      setCurrentSlide(id);
+                    }}
+                    href="#"
+                    scroll={false}
+                  >
+                    {text}
+                  </Link>
+                  <Link
+                    className={classNames({
+                      "text-sm rounded-full border px-4 py-2": true,
+                      hidden: !isActive,
+                    })}
+                    href="#"
+                  >
+                    Więcej
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <ul className="flex items-center justify-between w-1/4">
+            {slides.map(({ id }) => (
+              <li key={id}>
+                <CircleButton
+                  onClick={() => {
+                    setCurrentSlide(id);
+                  }}
+                  isActive={currentSlide === id}
+                  text={id.toString()}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="self-end w-1/2 px-16">
+          <h3 className="mb-4">Dlaczego Wielmi?</h3>
+          <p>
+            Zadbamy o wszystko za Ciebie. Nie musisz się o nic martwić. Nasi specjaliści zainstalują Twój system,
+            aktywują go i pokażą, jak z niego korzystać
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
