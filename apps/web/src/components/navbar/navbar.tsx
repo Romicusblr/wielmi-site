e12f4c26e541a595a018293761a87e6e963e2b30
Raman Mastyka <raman.mastyka@gmail.com>
2024-02-04 23:30:22 +0100

    style(navbar): fix centering

--- apps/web/src/components/navbar/navbar.tsx
+++ apps/web/src/components/navbar/navbar.tsx
@@ -13,7 +13,7 @@ const navigation = [
 const NavBar: FC = function () {
   return (
     <div className="w-full sticky top-0 z-10 bg-gray-200">
-      <div className="container flex flex-col items-center">
+      <div className="container mx-auto flex flex-col items-center">
         <h2 className="text-4xl my-4">WIELMI</h2>
         <hr className="h-px bg-gray-500 border-0 self-stretch" />
         <nav className="flex justify-between">
