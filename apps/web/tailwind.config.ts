a58f3d87e8607ed9209a0f5e9e9f02282fe51ba9
Raman Mastyka <raman.mastyka@gmail.com>
2024-04-21 20:59:59 +0200

    feat: update tailwind config

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,9 +3,28 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "content" | "presets"> = {
+const config: Pick<Config, "content" | "presets" | "theme"> = {
   content: ["./src/**/*.tsx"],
   presets: [sharedConfig],
+  theme: {
+    extend: {
+      screens: {
+        // 'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        // 'md': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        // 'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        // 'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1480px",
+      },
+    },
+  },
 };
 
 export default config;
