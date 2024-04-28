db5c056a2606d3c0bc489e66090f9847a5f6d947
Raman Mastyka <raman.mastyka@gmail.com>
2024-04-28 23:19:38 +0200

    fix: add navbar links

--- apps/web/src/components/navbar/navbar.tsx
+++ apps/web/src/components/navbar/navbar.tsx
@@ -9,58 +9,67 @@ const navigation = [
   { name: "Instalacje elektryczne", href: "#", current: false },
   { name: "Sieci lokalne", href: "#", current: false },
   { name: "Współpraca", href: "#", current: false },
-  { name: "O nas", href: "#", current: false },
+  { name: "Kontakty", href: "#", current: false },
 ];
 
 const NavBar: FC = function () {
   const [isOpen, setIsOpen] = useState(false);
-  const toggleOpen = (): void => { setIsOpen(!isOpen); };
+  const toggleOpen = (): void => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className="z-20 fixed top-0 right-0 flex items-center">
-      <button
-        aria-controls="mobile-menu"
-        aria-expanded="false"
-        className="relative sm:w-[10vw] p-2 inline-flex items-center justify-center rounded-md text-gray-400 hover:bg-gray-700 hover:text-white"
-        onClick={toggleOpen}
-        type="button"
+    <nav>
+      <div className="z-30 fixed top-0 right-0 flex items-center">
+        <button
+          aria-controls="mobile-menu"
+          aria-expanded="false"
+          className="relative sm:w-[10vw] p-2 inline-flex items-center justify-center rounded-md text-gray-400 hover:bg-gray-700 hover:text-white"
+          onClick={toggleOpen}
+          type="button"
+        >
+          <span className="sr-only">Open main menu</span>
+
+          <svg
+            aria-hidden="true"
+            className={classNames("h-12", isOpen ? "hidden" : "block")}
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            viewBox="0 0 24 24"
+          >
+            <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+
+          <svg
+            aria-hidden="true"
+            className={classNames("h-12", isOpen ? "block" : "hidden")}
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            viewBox="0 0 24 24"
+          >
+            <path d="M6 18L18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        </button>
+      </div>
+      <div
+        className={classNames(
+          "z-20 fixed inset-0 bg-black bg-opacity-75 transition-opacity grid lg:grid-cols-[10vw_1fr_1fr_10vw] sm:grid-cols-[10vw_1fr_10vw]",
+          isOpen ? "block" : "hidden"
+        )}
       >
-        <span className="sr-only">Open main menu</span>
-
-        <svg
-          aria-hidden="true"
-          className={classNames("h-12", isOpen ? "hidden" : "block")}
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          viewBox="0 0 24 24"
-        >
-          <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
-
-        <svg
-          aria-hidden="true"
-          className={classNames("h-12", isOpen ? "block" : "hidden")}
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          viewBox="0 0 24 24"
-        >
-          <path d="M6 18L18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
-      </button>
-    </div>
-    // <div className="container mx-auto flex flex-col items-center">
-    //   <nav className="flex justify-between">
-    //     {navigation.map(({ name, href }) => {
-    //       return (
-    //         <Link className="mt-4 mx-8" href={href} key={name}>
-    //           {name}
-    //         </Link>
-    //       );
-    //     })}
-    //   </nav>
-    // </div>
+        <div className="p-16 flex flex-col justify-center lg:col-start-3 sm:col-start-2">
+          {navigation.map(({ name, href }) => {
+            return (
+              <Link className="mt-4 text-gray-300 hover:text-gray-50" href={href} key={name}>
+                {name}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </nav>
   );
 };
 
