d2a06fecfc69b31bf34d1adb8cdd6e1b5aa8f3a7
Raman Mastyka <raman.mastyka@gmail.com>
2024-07-07 17:37:21 +0200

    fix: update hr

--- src/ui/hero.tsx
+++ src/ui/hero.tsx
@@ -5,24 +5,18 @@ import { faFacebookF, faInstagram, faWhatsapp } from "@fortawesome/free-brands-s
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BrandedButton from "./button";
+import GridLineH from "./line-horizontal";
 
 const Hero: FC = function () {
   return (
     <div className="relative w-full h-screen grid-layout grid-rows-6 text-dark-grey">
-      <Image
-        alt="cozy room"
-        className="-z-10 object-cover"
-        fill
-        priority
-        quality={100}
-        src="/wielmi-1.png"
-      />
+      <Image alt="cozy room" className="-z-10 object-cover" fill priority quality={100} src="/wielmi-1.png" />
       <div className="text-6xl col-span-2 col-start-2 row-start-3 flex flex-col lg:pl-36 justify-end">
         <h2>PROJEKTUJEMY</h2>
         <h2>I REALIZUJEMY</h2>
       </div>
       <div className="col-span-full row-start-4 hidden sm:flex flex-col justify-center">
-        <hr />
+        <GridLineH />
       </div>
       <div className="flex justify-center gap-4 col-start-2 h-12 row-start-5">
         <Link className="bg-dark-grey hover:bg-brand rounded-full p-2" href="#">
