c7957245e5335ce257b32a203d2aa8a23d385a4a
Raman Mastyka <raman.mastyka@gmail.com>
2024-07-07 19:47:17 +0200

    feat: add translate effect to cards

--- src/app/landing.tsx
+++ src/app/landing.tsx
@@ -36,7 +36,7 @@ interface CardProps {
 
 const Card: FC<CardProps> = function ({ num, heading, text }) {
   return (
-    <article className="p-4 pt-12 w-1/6 bg-[#E4F0FF] text-sm shadow-lg relative">
+    <article className="p-4 pt-12 w-1/6 bg-[#E4F0FF] text-sm shadow-lg relative transition-transform duration-200 hover:-translate-y-2 transform">
       <p className="absolute left-1 top-1 text-3xl text-muted-grey font-bold">{num}</p>
       <h3 className="font-semibold mb-4">{heading}</h3>
       <p>{text}</p>
@@ -46,46 +46,48 @@ const Card: FC<CardProps> = function ({ num, heading, text }) {
 
 const Landing: FC = function () {
   return (
-    <div className="grid-layout">
-      <div className="col-start-2 p-12">
-        <h2 className="text-2xl font-bold underline underline-offset-8 decoration-brand decoration-4">O Wielmi</h2>
-      </div>
-      <div className="p-12">
-        <h4 className="text-xl mb-4">Inteligentny dom Wielmi</h4>
-        <p>
-          Firma Wielmi to gwarancja kompleksowej i profesjonalnej realizacji projektów. Oferujemy pełen zakres usług,
-          obejmujących:
-        </p>
-        <ul className="list-disc ml-8">
-          <li>Projektowanie</li>
-          <li>Instalacje elektryczne</li>
-          <li>Uruchamianie i programowanie</li>
-          <li>Owarzancja i serwis</li>
-        </ul>
-        <p>
-          Nasze rozwiązania „pod klucz” zapewniają, że każda faza projektu jest wykonywana przez nasz zespół, co
-          gwarantuje spójność i najwyższą jakość. Dbamy o każdy szczegół, aby końcowy efekt spełniał Państwa
-          oczekiwania. Wybierając Wielmi, wybieracie Państwo jakość i terminowość.
-        </p>
-      </div>
-      <GridLineH className="col-span-full" />
-      <div className="col-start-2 p-12">
-        <h2 className="text-2xl font-bold underline underline-offset-8 decoration-brand decoration-4">
-          NASZA INSPIRACJA
-        </h2>
-      </div>
-      <div className="p-12">
-        <p className="mb-4">
-          Inspirują nas oryginalne projekty designerów, którzy nie trzymają się standardowych rozwiązań, a w swojej
-          pracy wykorzystują kreatywność i niestandardowe podejście.
-        </p>
-        <p className="mb-16">Jesteśmy otwarci na nowe projekty i gotowi do współpracy!</p>
-        <div className="flex justify-center">
-          <BrandedButton className="px-12">Czy masz jakiś pomysł?</BrandedButton>
+    <>
+      <section className="grid-layout">
+        <div className="col-start-2 p-12">
+          <h2 className="text-2xl font-bold underline underline-offset-8 decoration-brand decoration-4">O Wielmi</h2>
         </div>
-      </div>
-      <GridLineH className="col-span-full" />
-      <div className="col-span-full grid-layout bg-[#F6FAFF]">
+        <div className="p-12">
+          <h4 className="text-xl mb-4">Inteligentny dom Wielmi</h4>
+          <p>
+            Firma Wielmi to gwarancja kompleksowej i profesjonalnej realizacji projektów. Oferujemy pełen zakres usług,
+            obejmujących:
+          </p>
+          <ul className="list-disc ml-8">
+            <li>Projektowanie</li>
+            <li>Instalacje elektryczne</li>
+            <li>Uruchamianie i programowanie</li>
+            <li>Owarzancja i serwis</li>
+          </ul>
+          <p>
+            Nasze rozwiązania „pod klucz” zapewniają, że każda faza projektu jest wykonywana przez nasz zespół, co
+            gwarantuje spójność i najwyższą jakość. Dbamy o każdy szczegół, aby końcowy efekt spełniał Państwa
+            oczekiwania. Wybierając Wielmi, wybieracie Państwo jakość i terminowość.
+          </p>
+        </div>
+        <GridLineH className="col-span-full" />
+        <div className="col-start-2 p-12">
+          <h2 className="text-2xl font-bold underline underline-offset-8 decoration-brand decoration-4">
+            NASZA INSPIRACJA
+          </h2>
+        </div>
+        <div className="p-12">
+          <p className="mb-4">
+            Inspirują nas oryginalne projekty designerów, którzy nie trzymają się standardowych rozwiązań, a w swojej
+            pracy wykorzystują kreatywność i niestandardowe podejście.
+          </p>
+          <p className="mb-16">Jesteśmy otwarci na nowe projekty i gotowi do współpracy!</p>
+          <div className="flex justify-center">
+            <BrandedButton className="px-12">Czy masz jakiś pomysł?</BrandedButton>
+          </div>
+        </div>
+      </section>
+      <GridLineH />
+      <section className="col-span-full grid-layout bg-[#F6FAFF]">
         <div className="col-start-3 p-12">
           <h2 className="text-2xl font-bold underline underline-offset-8 decoration-brand decoration-4">ETAPY PRACY</h2>
         </div>
@@ -95,30 +97,9 @@ const Landing: FC = function () {
             return <Card num={num} key={i} heading={heading} text={text} />;
           })}
         </div>
-      </div>
-      {/* <div className="bg-white p-6 shadow rounded-lg">
-          <h2 className="text-lg font-bold mb-4">NASZA INSPIRACJA</h2>
-          <p className="text-gray-600">
-            Inspiurują nas oryginalne projekty designerów, którzy nie trzymają się standardowych rozwiązań, a w swojej
-            pracy wykorzystują kreatywność i nowoczesne podejście.
-          </p>
-          <button className="mt-4 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700">
-            Czy masz jakiś pomysł?
-          </button>
-        </div>
-
-        <div className="bg-white p-6 shadow rounded-lg">
-          <h2 className="text-lg font-bold mb-4">ETAPY PRACY</h2>
-          <ol className="list-decimal ml-4">
-            <li>Opacowanie koncepcji</li>
-            <li>Wykonanie projektu</li>
-            <li>Dobór i dostawa sprzętu</li>
-            <li>Wykonanie instalacji</li>
-            <li>Programowanie i uruchomienie</li>
-            <li>Gwarancje i serwisowe usługi</li>
-          </ol>
-        </div> */}
-    </div>
+      </section>
+      <GridLineH />
+    </>
   );
 };
 
