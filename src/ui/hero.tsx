7bff598c9f2bed24369b5beacb125a8a158080d4
Raman Mastyka <raman.mastyka@gmail.com>
2024-07-20 14:49:17 +0200

    feat: make hero responsible

--- src/ui/hero.tsx
+++ src/ui/hero.tsx
@@ -9,14 +9,14 @@ const Hero: FC = function () {
   return (
     <div className="relative w-full h-screen grid-layout grid-rows-6 text-dark-grey">
       <Image alt="cozy room" className="-z-10 object-cover" fill priority quality={100} src="/wielmi-1.png" />
-      <div className="text-6xl col-span-2 col-start-2 row-start-3 flex flex-col lg:pl-36 justify-end">
+      <div className="sm:col-start-2 lg:col-end-4 row-start-3 flex flex-col justify-end mx-auto lg:pr-72 sm:text-5xl lg:text-6xl text-4xl">
         <h2>PROJEKTUJEMY</h2>
         <h2>I REALIZUJEMY</h2>
       </div>
       <div className="col-span-full row-start-4 hidden sm:flex flex-col justify-center">
         <GridLineH />
       </div>
-      <div className="flex justify-center gap-4 col-start-2 h-12 row-start-5">
+      <div className="flex justify-center gap-4 sm:col-start-2 h-12 row-start-5">
         <Link className="bg-dark-grey hover:bg-brand rounded-full p-2" href="#">
           <FaFacebookF className="h-8 w-8 text-grey" />
         </Link>
@@ -30,7 +30,7 @@ const Hero: FC = function () {
           <FaEnvelope className="h-8 w-8 text-grey" />
         </Link>
       </div>
-      <div className="flex h-12 justify-center lg:row-start-5 lg:col-start-3 col-start-2 row-start-6">
+      <div className="flex h-12 justify-center lg:row-start-5 lg:col-start-3 sm:col-start-2 row-start-6">
         <BrandedButton className="px-12">napisz do nas</BrandedButton>
       </div>
     </div>
