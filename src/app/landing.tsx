e7bbbf5e3c5476396db70e7118571ad91aeb433e
Raman Mastyka <raman.mastyka@gmail.com>
2024-07-21 16:49:53 +0200

    feat: make infos section responsive

--- src/app/landing.tsx
+++ src/app/landing.tsx
@@ -10,6 +10,7 @@ import AboutSection from "@/ui/landing/about";
 import WorkStepsSection from "@/ui/landing/steps";
 import BlackCardSection from "@/ui/landing/black-cards";
 import IconCardSection from "@/ui/landing/icon-cards";
+import InfoSection from "@/ui/landing/info";
 
 
 const Landing: FC = function () {
@@ -23,76 +24,8 @@ const Landing: FC = function () {
       <BlackCardSection />
       <GridLineH />
       <IconCardSection />
+      <InfoSection />
       {/*
-
-      <section className="w-full pt-8 bg-grey text-dark-grey grid-layout">
-        <GridLineH className="mt-24 col-span-full" />
-        <GridLineH className="mt-12 col-span-full" />
-        <ResponsiveImage className="h-[48rem] col-span-2" src="/landing_id.jpeg" alt="intelegentny dom" />
-        <article className="px-12 pt-16">
-          <h2 className="text-3xl font-bold underline underline-offset-8 decoration-brand decoration-4 mb-8">
-            INTELIGENTNY DOM
-          </h2>
-          <p className="text-3xl mb-8">ZARZĄDZANIE DOMEM Z DOWOLNEGO MIEJSCA NA ŚWIECIE</p>
-          <p className="mb-8">
-            Inteligentny dom to system, który łączy różne urządzenia elektroniczne i systemy w domu pod jednym
-            zarządzaniem. Może to obejmować kontrolę oświetlenia, ogrzewania, klimatyzacji, bezpieczeństwa, systemów
-            multimedialnych i wielu innych.
-          </p>
-          <p>
-            Działanie wszystkich systemów w harmonii między sobą pozwala poczuć, czym jest komfort i wyrobić w sobie
-            nawyk jego odczuwania - poczucie spokoju i pewności, że wszystko jest pod kontrolą.{" "}
-          </p>
-        </article>
-        <GridLineH className="col-span-full" />
-        <article className="px-12 pt-16 col-start-2">
-          <h2 className="text-2xl font-bold underline underline-offset-8 decoration-brand decoration-4 mb-8">
-            INSTALACJE ELEKTRYCZNE
-          </h2>
-          <p className="mb-4">
-            Nasze podejście do instalacji elektrycznych to połączenie profesjonalizmu z elastycznością, aby sprostać
-            Twoim oczekiwaniom. Rozumiemy, że technologia jest w ciągłym ruchu i nasza praca powinna wspierać ten
-            rozwój.
-          </p>
-          <ol className="list-inside list-decimal mb-4">
-            Nasze Zalety:
-            <li>Wysoka Jakość Wykonania;</li>
-            <li>Profesjonalizm i Doświadczenie;</li>
-            <li>Bezpieczeństwo;</li>
-            <li>Terminowość;</li>
-            <li>Innowacyjność;</li>
-            <li>Transparentność.</li>
-          </ol>
-          <p>
-            Wybierając naszą firmę, masz pewność, że otrzymasz kompleksowe, nowoczesne i niezawodne rozwiązania
-            dostosowane do Twoich potrzeb.
-          </p>
-        </article>
-        <ResponsiveImage
-          className="h-[48rem] col-span-2 col-start-3"
-          src="/landing_electric.png"
-          alt="intelegentny dom"
-        />
-        <GridLineH className="col-span-full" />
-        <ResponsiveImage className="h-[48rem] col-span-2" src="/landing_lan.png" alt="intelegentny dom" />
-        <article className="px-12 pt-16">
-          <h2 className="text-3xl font-bold underline underline-offset-8 decoration-brand decoration-4 mb-8">
-            SIECI LOKALNE
-          </h2>
-          <p className="text-3xl mb-8">ZINTEGROWANE ROZWIĄZANIA TECHOLOGICZNNE</p>
-          <p className="mb-8">
-            Specjalizujemy się w tworzeniu kompleksowych rozwiązań zapewniających ciągłą i wysokiej jakości pracę
-            systemów w Twoim domu.
-          </p>
-          <p>
-            Otrzymujesz komfort i technologię bez konieczności zagłębiania się w zawiłości szczegółów technicznych.
-            Wyobraź sobie na przykład, że możesz stworzyć atmosferę w swoim domu na imprezę, romantyczną kolację lub
-            rodzinny wieczór filmowy, nie ruszając się z kanapy.
-          </p>
-        </article>
-        <GridLineH className="col-span-full" />
-        <GridLineH className="mt-12 col-span-full" />
-      </section>
       <section className="w-full py-8 bg-grey text-dark-grey grid-layout">
         <div className="text-3xl col-start-2 p-8">
           <h2 className="font-bold underline underline-offset-8 decoration-brand decoration-4 mb-8">NASZ ZESPÓŁ</h2>
---
+++ src/ui/landing/info.tsx
@@ -0,0 +1,110 @@
+import type { FC } from "react";
+import GridLineH from "@/ui/line-horizontal";
+import ResponsiveImage from "../responsive-image";
+import classNames from "classnames";
+
+const infos = [
+  {
+    imageSrc: "/landing_id.jpeg",
+    heading: "INTELIGENTNY DOM",
+    article: (
+      <>
+        <p className="text-2xl mb-8">ZARZĄDZANIE DOMEM Z DOWOLNEGO MIEJSCA NA ŚWIECIE</p>
+        <p className="mb-8">
+          Inteligentny dom to system, który łączy różne urządzenia elektroniczne i systemy w domu pod jednym
+          zarządzaniem. Może to obejmować kontrolę oświetlenia, ogrzewania, klimatyzacji, bezpieczeństwa, systemów
+          multimedialnych i wielu innych.
+        </p>
+        <p>
+          Działanie wszystkich systemów w harmonii między sobą pozwala poczuć, czym jest komfort i wyrobić w sobie nawyk
+          jego odczuwania - poczucie spokoju i pewności, że wszystko jest pod kontrolą.{" "}
+        </p>
+      </>
+    ),
+  },
+  {
+    imageSrc: "/landing_electric.png",
+    heading: "INSTALACJE ELEKTRYCZNE",
+    article: (
+      <>
+        <p className="mb-4">
+          Nasze podejście do instalacji elektrycznych to połączenie profesjonalizmu z elastycznością, aby sprostać Twoim
+          oczekiwaniom. Rozumiemy, że technologia jest w ciągłym ruchu i nasza praca powinna wspierać ten rozwój.
+        </p>
+        <ol className="list-inside list-decimal mb-4">
+          Nasze Zalety:
+          <li>Wysoka Jakość Wykonania;</li>
+          <li>Profesjonalizm i Doświadczenie;</li>
+          <li>Bezpieczeństwo;</li>
+          <li>Terminowość;</li>
+          <li>Innowacyjność;</li>
+          <li>Transparentność.</li>
+        </ol>
+        <p>
+          Wybierając naszą firmę, masz pewność, że otrzymasz kompleksowe, nowoczesne i niezawodne rozwiązania
+          dostosowane do Twoich potrzeb.
+        </p>
+      </>
+    ),
+  },
+  {
+    imageSrc: "/landing_lan.png",
+    heading: "SIECI LOKALNE",
+    article: (
+      <>
+        <p className="text-3xl mb-8">ZINTEGROWANE ROZWIĄZANIA TECHOLOGICZNNE</p>
+        <p className="mb-8">
+          Specjalizujemy się w tworzeniu kompleksowych rozwiązań zapewniających ciągłą i wysokiej jakości pracę systemów
+          w Twoim domu.
+        </p>
+        <p>
+          Otrzymujesz komfort i technologię bez konieczności zagłębiania się w zawiłości szczegółów technicznych.
+          Wyobraź sobie na przykład, że możesz stworzyć atmosferę w swoim domu na imprezę, romantyczną kolację lub
+          rodzinny wieczór filmowy, nie ruszając się z kanapy.
+        </p>
+      </>
+    ),
+  },
+];
+
+interface InfoProps {
+  imageSrc: string;
+  heading: string;
+  article: JSX.Element;
+  reverse?: boolean;
+}
+
+const Info: FC<InfoProps> = function ({ imageSrc, heading, article, reverse }) {
+  const classImage = reverse ? "lg:col-start-3" : "";
+  const classArticle = reverse ? "lg:col-start-2" : "lg:col-start-3";
+
+  return (
+    <>
+      <ResponsiveImage className={classNames("lg:h-[48rem] h-[36rem] lg:col-span-2 col-span-full", classImage)} src={imageSrc} alt={heading} />
+      <article className={classNames("px-12 py-16 lg:col-start-2 sm:col-start-2", classArticle)}>
+        <h2 className="text-3xl font-bold underline underline-offset-8 decoration-brand decoration-4 mb-8">
+          {heading}
+        </h2>
+        {article}
+      </article>
+    </>
+  );
+};
+
+const InfoSection: FC = function () {
+  return (
+    <section className="w-full pt-8 bg-grey text-dark-grey grid-layout grid-flow-dense">
+      <GridLineH className="mt-12 col-span-full" />
+      <GridLineH className="mt-12 col-span-full" />
+      <Info {...infos[0]} />
+      <GridLineH className="col-span-full" />
+      <Info {...infos[1]} reverse/>
+      <GridLineH className="col-span-full" />
+      <Info {...infos[2]} />
+      <GridLineH className="col-span-full" />
+      <GridLineH className="mt-12 col-span-full" />
+    </section>
+  );
+};
+
+export default InfoSection;
