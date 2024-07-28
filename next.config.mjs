2ea8192e2cca5abe2520766d2c33e1164ccf164e
Raman Mastyka <raman.mastyka@gmail.com>
2024-07-28 12:30:40 +0200

    feat: add analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 import withExportImages from "next-export-optimize-images";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
-const nextConfig = withExportImages(
-  {
+const nextConfig = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(
+  withExportImages({
     output: "export",
     basePath: process.env.BASE_PATH ?? "",
-  }
+  })
 );
 
 export default nextConfig;
