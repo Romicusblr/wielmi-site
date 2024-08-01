931c08283b302d80677b9b88d99d9afeb575ea2a
Raman Mastyka <raman.mastyka@gmail.com>
2024-08-01 19:57:09 +0200

    fix: update padding

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -4,8 +4,8 @@ import Logo from "./logo";
 
 const NavBar: FC = function () {
   return (
-    <nav className="fixed z-30 w-full text-dark-grey bg-grey bg-opacity-90 px-2 sm:px-0">
-      <div className="grid place-items-center p-4">
+    <nav className="p-2 fixed z-30 w-full text-dark-grey bg-grey bg-opacity-90 px-2 sm:px-0">
+      <div className="grid place-items-center">
         <div className="h-12 w-24 sm:w-[10vw] p-2">
           <Logo />
         </div>
