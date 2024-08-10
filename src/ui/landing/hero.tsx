22b8cf39bc682e46c48cad797030e6056514ff4c
Raman Mastyka <raman.mastyka@gmail.com>
2024-08-10 21:38:43 +0200

    fix: update hero positioning

--- src/ui/landing/hero.tsx
+++ src/ui/landing/hero.tsx
@@ -4,9 +4,11 @@ import BrandedButton from "@/ui/button";
 import GridLineH from "@/ui/line-horizontal";
 import SocialLinks from "@/ui/social-links";
 import Link from "next/link";
+import classnames from "classnames";
 import { CONTACT_HASH } from "@/constants";
 
 const Hero: FC = function () {
+  const paddingLeft = "2xl:pl-60 xl:pl-48 lg:pl-36";
   return (
     <div className="relative w-full h-screen grid-layout grid-rows-6 text-dark-grey">
       <Image
@@ -18,14 +20,19 @@ const Hero: FC = function () {
         src="/images/wielmi-1.png"
         sizes="100%"
       />
-      <div className="sm:col-start-2 lg:col-end-4 row-start-3 flex flex-col justify-end mx-auto lg:pr-72 sm:text-5xl lg:text-6xl text-4xl">
+      <div
+        className={classnames(
+          paddingLeft,
+          "sm:col-start-2 lg:col-end-4 row-start-3 flex flex-col items-center lg:items-stretch justify-end sm:text-6xl text-4xl 2xl:pl-64"
+        )}
+      >
         <h2>PROJEKTUJEMY</h2>
         <h2>I REALIZUJEMY</h2>
       </div>
       <div className="col-span-full row-start-4 hidden sm:flex flex-col justify-center">
         <GridLineH />
       </div>
-      <div className="h-12 flex justify-center gap-4 sm:col-start-2 row-start-5">
+      <div className={classnames(paddingLeft, "h-12 flex justify-center lg:justify-start gap-4 sm:col-start-2 row-start-5")}>
         <SocialLinks iconClassName="h-8 w-8" />
       </div>
       <div className="h-12 flex justify-center lg:row-start-5 lg:col-start-3 sm:col-start-2 row-start-6">
