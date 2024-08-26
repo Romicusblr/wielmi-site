9fb070baedebcab43c02e85513d6be7a68b0d720
Raman Mastyka <raman.mastyka@gmail.com>
2024-08-27 00:40:37 +0200

    feat: add o nas page

---
+++ src/app/o-nas/section1.tsx
@@ -0,0 +1,14 @@
+import SocialLinks from "@/ui/social-links";
+import type { FC } from "react";
+
+const Section1: FC = () => {
+  return (
+    <section className="grid-layout">
+      <div className="sm:col-start-2 p-12">
+        <h1 className="text-4xl font-bold underline underline-offset-8 decoration-brand decoration-4">O Wielmi</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Section1;
