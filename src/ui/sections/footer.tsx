4fd1c8f7bcaee8e1bdf83dbec19c42800ae580df
Raman Mastyka <raman.mastyka@gmail.com>
2024-08-29 22:54:20 +0200

    fix: update footer

--- src/ui/sections/footer.tsx
+++ src/ui/sections/footer.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 const Footer: FC = function () {
   return (
     <footer className="bg-grey min-h-96 grid-layout text-dark-grey">
-      <div className="sm:col-start-2 lg:col-end-4 flex sm:flex-row flex-col justify-between">
-        <div className="flex flex-col lg:p-24 p-12 justify-center">
+      <div className="p-6 sm:col-start-2 flex justify-center items-center">
+        <div>
           <h2 className="text-3xl underline underline-offset-8">KONTAKTY</h2>
           <p className="mt-4 text-brand">INTELIGENTNY DOM Wielmi</p>
           <p className="mt-8">Kraków, Polska</p>
@@ -14,16 +14,18 @@ const Footer: FC = function () {
             <SocialLinks />
           </div>
         </div>
-        <div className="flex flex-col lg:p-24 p-12 justify-center">
+      </div>
+      <div className="p-6 sm:col-start-2 lg:col-start-3 flex justify-center items-center">
+        <div>
           <p>Godziny pracy: od 10:00 do 19:00</p>
           <p>Weekendy: sobota, niedziela</p>
-          <p className="mt-4">
+          <p className="mt-4 flex justify-center">
             <a href="tel:+48571780770">+ 48 571 780 770</a>
           </p>
-          <p className="mt-4">
+          <p className="mt-4 flex justify-center">
             <a href="mailto:kontakt@wielmi.pl">kontakt@wielmi.pl</a>
           </p>
-          <p className="mt-4">
+          <p className="mt-4 flex justify-center">
             <Link href="/polityka-prywatnosci">Polityka Prywatności</Link>
           </p>
         </div>
