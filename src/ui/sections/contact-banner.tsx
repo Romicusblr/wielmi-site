5710ff248e313694adc1db603d8c3bad0ca32161
Raman Mastyka <raman.mastyka@gmail.com>
2024-08-29 22:56:02 +0200

    fix: update contact banner

--- src/ui/sections/contact-banner.tsx
+++ src/ui/sections/contact-banner.tsx
@@ -8,15 +8,14 @@ const ContactBanner = () => {
         alt="Background"
         className="col-span-full grid-layout"
       >
-
-      <div className="sm:col-start-3 lg:col-end-5 flex flex-col justify-center p-8 z-10">
-        <h2 className="text-3xl font-bold text-white ">Skontaktuj się z nami</h2>
-        <div className="w-16 h-1 bg-blue-500 mt-2 mb-4"></div>
-        <p className="text-xl text-gray-300">
-          BEZPOŚREDNIO <br />
-          LUB TELEFONICZNIE
-        </p>
-      </div>
+        <div className="sm:col-start-2 lg:col-start-3 lg:col-end-5 flex flex-col justify-center p-8 z-10">
+          <h2 className="text-3xl font-bold text-white ">Skontaktuj się z nami</h2>
+          <div className="w-16 h-1 bg-blue-500 mt-2 mb-4"></div>
+          <p className="text-xl text-gray-300">
+            BEZPOŚREDNIO <br />
+            LUB TELEFONICZNIE
+          </p>
+        </div>
       </ResponsiveImage>
     </section>
   );
