78bdc429bf7d9205af5c1e58b53af1f3da1fe411
Raman Mastyka <raman.mastyka@gmail.com>
2024-09-03 22:22:46 +0200

    fix: update landing styles

--- src/ui/landing/hero.tsx
+++ src/ui/landing/hero.tsx
@@ -6,6 +6,9 @@ import SocialLinks from "@/ui/sections/social-links";
 import Link from "next/link";
 import classnames from "classnames";
 import { CONTACT_HASH } from "@/constants";
+import { Inter } from "next/font/google";
+
+const font = Inter({ subsets: ["latin", "latin-ext"] });
 
 const Hero: FC = function () {
   const paddingLeft = "2xl:pl-60 xl:pl-48 lg:pl-36";
@@ -20,22 +23,20 @@ const Hero: FC = function () {
         src="/images/hero-bg.png"
         sizes="100%"
       />
-      <div
-        className={classnames(
-          paddingLeft,
-          "sm:col-start-2 lg:col-end-4 row-start-3 flex flex-col items-center lg:items-stretch justify-end 2xl:text-7xl sm:text-6xl text-4xl"
-        )}
-      >
-        <h2>PROJEKTUJEMY</h2>
-        <h2>I REALIZUJEMY</h2>
-      </div>
+
       <div className="col-span-full row-start-4 hidden sm:flex flex-col justify-center">
         <GridLineH />
       </div>
-      <div className={classnames(paddingLeft, "h-12 flex justify-center lg:justify-start gap-4 sm:col-start-2 row-start-5")}>
-        <SocialLinks iconClassName="h-8 w-8" />
+      <div className={classnames("h-16 flex justify-center items-center gap-4 sm:col-start-2 row-start-5")}>
+        <div className="relative">
+          <div className={classnames(font.className, "2xl:text-7xl sm:text-6xl text-4xl absolute lg:left-0 -top-72 font-[500] tracking-wider")}>
+            <h2>PROJEKTUJEMY</h2>
+            <h2>I REALIZUJEMY</h2>
+          </div>
+          <SocialLinks iconClassName="h-8 w-8" />
+        </div>
       </div>
-      <div className="h-12 flex justify-center lg:row-start-5 lg:col-start-3 sm:col-start-2 row-start-6">
+      <div className="h-16 flex justify-center lg:row-start-5 lg:col-start-3 sm:col-start-2 row-start-6">
         <Link href={CONTACT_HASH}>
           <BrandedButton className="px-12 sm:w-96 w-full">napisz do nas</BrandedButton>
         </Link>
