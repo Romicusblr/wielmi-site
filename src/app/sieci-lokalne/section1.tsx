43a7e61c3aa89f36963133f05245471efcf73300
Raman Mastyka <raman.mastyka@gmail.com>
2024-09-10 22:56:15 +0200

    fix: update seci lokalne page

--- src/app/sieci-lokalne/section1.tsx
+++ src/app/sieci-lokalne/section1.tsx
@@ -3,13 +3,13 @@ import type { FC } from "react";
 
 const Section1: FC = () => {
   return (
-    <section className="grid-layout">
-      <div className="sm:col-start-2 p-12">
-        <Heading title="SIECI LOKALNE" subtitles={["ZINTEGROWANE", "ROZWIĄZANIA", "TECHNOLOGICZNE"]} />
+    <section className="grid-layout py-20">
+      <div className="sm:col-start-2 px-12">
+        <Heading title={<span>SIECI <br /> LOKALNE</span>} subtitles={["ZINTEGROWANE", "ROZWIĄZANIA", "TECHNOLOGICZNE"]} />
       </div>
-      <div className="sm:col-start-2 lg:col-start-3 p-12 pt-0 lg:pt-12">
-        <h3 className="text-xl font-bold mb-4">CO TO JEST SIECI LOKALNE?</h3>
-        <p className="mb-4">
+      <div className="sm:col-start-2 lg:col-start-3 px-12">
+        <h3 className="text-xl font-bold">CO TO JEST SIECI LOKALNE?</h3>
+        <p className="mt-4">
           W dzisiejszym świecie, gdzie każdy aspekt naszego życia jest powiązany z technologią, zintegrowane systemy
           komunikacji i przesyłania danych stają się kluczowym elementem każdego nowoczesnego domu. Od domowych kin po
           systemy monitoringu i inteligentne sieci danych - oferujemy kompleksowe rozwiązania, które wyposażą Twój dom w
