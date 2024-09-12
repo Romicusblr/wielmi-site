02d9a63f835a19f96cbd1b078e5aedeff472f262
Raman Mastyka <raman.mastyka@gmail.com>
2024-09-12 22:58:13 +0200

    fix: rename sections

--- src/app/instalacje-elektryczne/page.tsx
+++ src/app/instalacje-elektryczne/page.tsx
@@ -1,18 +1,18 @@
 import type { FC } from "react";
 import { GridLineH } from "@/ui/sections/grid-lines";
-import Section1 from "./section1";
-import Section2 from "./section2";
+import Description from "./description";
+import InfoSection from "./infos";
 import ContactSection from "@/ui/sections/contact";
 import ArrowDownSection from "@/ui/sections/arrow-down-section";
 
 const Page: FC = function () {
   return (
     <>
-      <Section1 />
+      <Description />
       <GridLineH />
       <ArrowDownSection />
       <GridLineH />
-      <Section2 />
+      <InfoSection />
       <ContactSection />
     </>
   );
