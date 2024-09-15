3163382f7c674f013a114435df260a39f107c821
Raman Mastyka <raman.mastyka@gmail.com>
2024-09-15 21:38:24 +0200

    refactor: udpate services

--- src/ui/landing/services.tsx
+++ src/ui/landing/services.tsx
@@ -22,10 +22,10 @@ const services = [
 
 const ServicesSection = () => {
   return (
-    <section className="w-full section py-12">
-      <div className="sm:col-start-2 lg:col-end-4 grid lg:grid-cols-2 lg:grid-flow-dense text-xl">
+    <section className="section">
+      <div className="content-center-span px-0 grid lg:grid-cols-2 lg:grid-flow-dense text-xl">
         {services.map((service, index) => (
-          <ServiceItem key={index} iconSize={28} {...service} className="px-12"/>
+          <ServiceItem key={index} iconSize={28} {...service} className="content-padding"/>
         ))}
       </div>
     </section>
