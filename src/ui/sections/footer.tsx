df1a86395e183f670183c1d57752461869e307e9
Raman Mastyka <raman.mastyka@gmail.com>
2024-09-15 23:23:54 +0200

    refactor: update footer, social links

--- src/ui/sections/footer.tsx
+++ src/ui/sections/footer.tsx
@@ -7,9 +7,9 @@ import Logo from "@/ui/common/logo";
 
 const Footer: FC = function () {
   return (
-    <footer className="bg-grey min-h-96 section text-dark-grey">
-      <div className="sm:col-start-2 flex lg:justify-start justify-center items-center p-12">
-        <div className="flex flex-col items-center lg:block">
+    <footer className="bg-grey text-dark-grey">
+      <div className="section">
+        <div className="content-left-half flex flex-col items-center lg:items-start">
           <Heading title="KONTAKTY" titleClass="!text-4xl font-normal lg:block flex flex-col items-center" />
           <p className="text-brand tracking-[0.1em] text-xl -mt-4">INTELIGENTNY DOM Wielmi</p>
           <p className="mt-8 text-3xl">Kraków, Polska</p>
@@ -17,27 +17,29 @@ const Footer: FC = function () {
             <SocialLinks />
           </div>
         </div>
-      </div>
-      <div className="p-12 sm:col-start-2 lg:col-start-3 flex justify-center items-end text-xl">
-        <div>
-          <p>Godziny pracy: od 10:00 do 19:00</p>
-          <p className="text-center lg:text-start">Weekendy: sobota, niedziela</p>
-          <p className="mt-4 flex justify-center lg:justify-start">
-            <a href="tel:+48571780770">+ 48 571 780 770</a>
-          </p>
-          <p className="mt-4 flex justify-center lg:justify-start">
-            <a href="mailto:kontakt@wielmi.pl">kontakt@wielmi.pl</a>
-          </p>
+        <div className="content-right-half flex justify-center items-end text-xl">
+          <div>
+            <p>Godziny pracy: od 10:00 do 19:00</p>
+            <p className="text-center lg:text-start">Weekendy: sobota, niedziela</p>
+            <p className="mt-4 flex justify-center lg:justify-start">
+              <a href="tel:+48571780770">+ 48 571 780 770</a>
+            </p>
+            <p className="mt-4 flex justify-center lg:justify-start">
+              <a href="mailto:kontakt@wielmi.pl">kontakt@wielmi.pl</a>
+            </p>
+          </div>
         </div>
       </div>
-      <GridLineH className="col-span-full" />
-      <div className="p-2">
-        <Logo />
-      </div>
-      <div className="sm:col-start-2 lg:col-start-3 p-4">
-        <p className="flex justify-center uppercase tracking-[0.05em]">
-          <Link href="/polityka-prywatnosci">Polityka Prywatności</Link>
-        </p>
+      <GridLineH />
+      <div className="section py-4">
+        <div className="p-2">
+          <Logo />
+        </div>
+        <div className="sm:col-start-2 lg:col-start-3 grid place-content-center p-2">
+          <p className="flex justify-center uppercase tracking-[0.05em]">
+            <Link href="/polityka-prywatnosci">Polityka Prywatności</Link>
+          </p>
+        </div>
       </div>
     </footer>
   );
