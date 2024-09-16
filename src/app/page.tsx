f52fe656703ed41de1efb0abe59932ddc18f07a3
Raman Mastyka <raman.mastyka@gmail.com>
2024-09-16 21:26:03 +0200

    refactor: remove separrate landing component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,42 @@
 import type { FC } from "react";
-import Landing from "./landing";
+import { GridLineH } from "@/ui/sections/grid-lines";
+import Testimonials from "@/ui/landing/testimonials";
+import Hero from "@/ui/landing/hero";
+import AboutSection from "@/ui/sections/about";
+import WorkStepsSection from "@/ui/landing/steps";
+import BlackCardSection from "@/ui/landing/black-cards";
+import ServicesSection from "@/ui/landing/services";
+import InfoSection from "@/ui/landing/info";
+import TeamSection from "@/ui/landing/team";
+import ContactSection from "@/ui/sections/contact";
+import StatisticsSection from "@/ui/landing/statistics";
+import ArrowDownSection from "@/ui/sections/arrow-down-section";
 
 const Page: FC = function () {
-  return <Landing />;
+  return (
+    <>
+      <Hero />
+      <GridLineH />
+      <AboutSection />
+      <GridLineH />
+      <WorkStepsSection />
+      <GridLineH />
+      <StatisticsSection />
+      <GridLineH />
+      <BlackCardSection />
+      <GridLineH />
+      <ServicesSection />
+      <GridLineH />
+      <ArrowDownSection className="bg-[#F5F5F5]"/>
+      <InfoSection />
+      <GridLineH />
+      <ArrowDownSection className="bg-grey"/>
+      <GridLineH />
+      <TeamSection />
+      <ContactSection />
+      <Testimonials />
+    </>
+  );
 };
 
 export default Page;
