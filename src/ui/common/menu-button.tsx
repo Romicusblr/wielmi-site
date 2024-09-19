a151b0eec3fcab03a5945fdad1bef39642c80fed
Raman Mastyka <raman.mastyka@gmail.com>
2024-09-19 21:54:37 +0200

    fix: update navbar

--- src/ui/common/menu-button.tsx
+++ src/ui/common/menu-button.tsx
@@ -1,25 +1,16 @@
 import type { FC } from "react";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 
 interface MenuButtonProps {
   onClick: () => void;
+  isOpen: boolean;
 }
 
-const MenuButton: FC<MenuButtonProps> = function ({ onClick }) {
+const MenuButton: FC<MenuButtonProps> = function ({ onClick, isOpen }) {
   return (
-    <button
-      onClick={onClick}
-      type="button"
-    >
+    <button onClick={onClick} type="button">
       <span className="sr-only">Open main menu</span>
-      <svg className="w-8 h-8" fill="none" viewBox="0 0 17 14" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M1 1h15M1 7h15M1 13h15"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-        />
-      </svg>
+      {isOpen ? <RxCross1 size={50} className="text-brand"/> : <RxHamburgerMenu size={50} />}
     </button>
   );
 };
