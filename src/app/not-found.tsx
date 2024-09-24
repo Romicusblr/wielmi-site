baefefb848f4bba61ceb800b2073b888729b9fee
Raman Mastyka <raman.mastyka@gmail.com>
2024-09-24 23:25:22 +0200

    fix: fix 404 page style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import { FaHome } from "react-icons/fa";
 
 const Custom404: React.FC = () => {
   return (
-    <div className="flex-1 flex flex-col h-full items-center justify-center p-10 z-10 bg-[#F5F5F5]">
+    <div className="min-h-[50vh] flex-1 flex flex-col h-full items-center justify-center p-10 z-10 bg-[#F5F5F5] text-center">
       <h1 className="text-6xl font-bold">404</h1>
       <h2 className="text-2xl mt-4">Oops! Strona, której szukasz, nie istnieje.</h2>
       <p className="text-lg mt-4 mb-8">Wygląda na to, że nie możemy znaleźć strony, której szukasz.</p>
