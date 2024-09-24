6a982791998bd0fdb04d52856d67826828f4c0fa
Raman Mastyka <raman.mastyka@gmail.com>
2024-09-24 22:48:00 +0200

    fix: fix metadata links

--- src/ui/common/meta.tsx
+++ src/ui/common/meta.tsx
@@ -1,26 +1,19 @@
 import { Metadata } from "next";
 
 export const siteMetadata: Metadata = {
+  metadataBase: new URL("https://wielmi.pl"),
   title: {
-    template: '%s | Wielmi Kraków',
-    default: 'Inteligentny Dom, Instalacje Elektryczne | Wielmi Kraków',
+    template: "%s | Wielmi Kraków",
+    default: "Inteligentny Dom, Instalacje Elektryczne | Wielmi Kraków",
   },
-  description: "Firma Wielmi oferuje instalacje elektryczne, inteligentne domy i sieci lokalne w Krakowie. Profesjonalne rozwiązania dla nowoczesnych domów.",
-  keywords: [
-    "Inteligentny dom Kraków", 
-    "Instalacje elektryczne", 
-    "Sieci lokalne", 
-    "Smart home", 
-    "Wielmi Kraków"
-  ],
+  description:
+    "Firma Wielmi oferuje instalacje elektryczne, inteligentne domy i sieci lokalne w Krakowie. Profesjonalne rozwiązania dla nowoczesnych domów.",
+  keywords: ["Inteligentny dom Kraków", "Instalacje elektryczne", "Sieci lokalne", "Smart home", "Wielmi Kraków"],
   applicationName: "Wielmi",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-  },
   openGraph: {
     title: "Inteligentny Dom - Wielmi Kraków",
-    description: "Profesjonalne instalacje elektryczne i inteligentne systemy domowe dla nowoczesnych domów w Krakowie.",
+    description:
+      "Profesjonalne instalacje elektryczne i inteligentne systemy domowe dla nowoczesnych domów w Krakowie.",
     url: "https://wielmi.pl",
     siteName: "Wielmi",
     locale: "pl_PL",
